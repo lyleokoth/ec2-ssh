50233d5c73ec3d73c0725efd0004c394ecc2da3f
lyleokoth <okothlyleochieng@gmail.com>
2022-06-05 15:51:43 +0300

    testing the workflo.

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@
 FROM circleci/sshd:0.1
 
 # Copies your code file from your action repository to the filesystem path `/` of the container
-COPY entrypoint.sh /entrypoint.sh
+COPY . .
 
 # Code file to execute when the docker container starts up (`entrypoint.sh`)
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,17 @@
 #!/bin/sh -l
 
-echo "Hello ${INPUT_WHO_TO_GREET}"
+echo "Connecting to the remote host..."
+
+echo ${PRIVATE_KEY} > private_key && chmod 600 private_key
+
+ssh -o StrictHostKeyChecking=no -i ${PRIVATE_KEY} ${INPUT_USER_NAME}@${INPUT_HOST_NAME} "
+
+    cd ${INPUT_APP_DIR} &&
+    git pull &&
+    echo ${INPUT_USER_PASSWORD} | sudo -S systemctl restart ${INPUT_SERVICE_NAME}
+"
+echo "Finished..."
+
 time=$(date)
+
 echo "::set-output name=time::$time"
